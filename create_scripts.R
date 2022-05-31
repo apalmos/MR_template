425f6114fdc995e6245406cb5bd9f23bd8b48ab5
apalmos <58469139+apalmos@users.noreply.github.com>
2022-05-31 12:21:35 +0100

    fixed a lsit error when making the array scripts & decreased CPU request

--- create_scripts.R
+++ create_scripts.R
@@ -6,6 +6,7 @@ rm(list=ls())
 library(data.table)
 library(tidyverse)
 library(vroom)
+library(rlist)
 
 
 ## --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -17,7 +18,6 @@ dirs = dirs[!(dirs %in% elements_2_remove)]
 n=1
 
 #slice decides by how many GWASs each script set will be split by; for example, if there are 600 GWASs and slice is set to 150, 4 different script sets will be generated
-
 slice=150
 
 #this loop screates all scripts, for each folder directory
@@ -25,20 +25,22 @@ for (j in dirs){
   all_prots <- vroom(paste0(dirs[n],"/full_list"),col_names = FALSE)
   repn <- round(nrow(all_prots)/slice)
   splitted <- split(all_prots,rep(1:paste0(repn),each=paste0(slice)))
+
+  #function to remove any empty lists
+  splitted <- list.clean(splitted, function(x) length(x) == 0L, TRUE)
   n_split <- length(splitted)
   sep <- c("'")
+
   for (i in 1:n_split){
 
-  #here we write the array, according to the slice vector
-
+    #here we write the array, according to the slice vector
     split_list <- as.data.frame(splitted[i])
     vroom_write(x = split_list, file = paste0(dirs[n],"/list",i,".txt"), col_names = FALSE, delim = ',')
     flag <- paste0("flag",i,".txt")
 
     #this script makes the initial slurm command for launching the array
-
     mr <- c("#!/bin/bash",
-            "#SBATCH --nodes=4",
+            "#SBATCH --nodes=2",
             "#SBATCH --mem-per-cpu=18G",
             "#SBATCH -t 48:00:00",
             " ",
@@ -64,7 +66,6 @@ for (j in dirs){
     writeLines(mr, paste0(dirs[n],"/MR",dirs_clean[n],i,".sh"))
 
     #this script does a little bit of cleaning of the GWASs and creates marker_name files corresponding to the GWAS being analysed. This makes future analyses easier as the protein name and code (for lookup in the metadata) will be available
-
     clean <- c("#!/bin/bash",
             " ",
             paste0("number=",i),
@@ -90,9 +91,8 @@ for (j in dirs){
     writeLines(clean, paste0(dirs[n],"/mass_mr",i,".sh"))
 
     #this script runs GSMR. The settings can be changed according to user needs. See the MR_template is any of the below dependencies are missing
-
     MR <- c("#!/bin/bash",
-            "#SBATCH --nodes=4",
+            "#SBATCH --nodes=1",
             "#SBATCH --mem-per-cpu=18G",
             "#SBATCH -t 48:00:00",
             " ",
